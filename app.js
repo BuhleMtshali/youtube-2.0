07ea4b8a83aaba213e3547020f0903477d0676a8
Buhle <mtshalizanobuhle7@gmail.com>
2025-03-21 11:07:47 +0200

    updated the javascript file

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+//toggling the sidebar
 document.getElementById('menu').addEventListener('click', () => {
     let sideBar = document.querySelector('.side-bar');
     sideBar.classList.toggle('hide')
+});
+
+//adding the active class to the nav-link
+let navLinkElements = document.querySelectorAll('.nav-link');
+navLinkElements.forEach((nav) => {
+    nav.addEventListener('click', () => {
+        navLinkElements.forEach((link) => link.classList.remove('active'));
+        nav.classList.add('active');
+    })
 })
