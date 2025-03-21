47c46c05f17025d0eb50e10b6b6bcde1169e2e38
Buhle <mtshalizanobuhle7@gmail.com>
2025-03-21 15:03:42 +0200

    updated app.js

--- app.js
+++ app.js
@@ -13,3 +13,57 @@ navLinkElements.forEach((nav) => {
         nav.classList.add('active');
     })
 })
+
+//calling getYoutube function
+document.addEventListener('DOMContentLoaded', () => {
+    fetchVideos();
+});
+
+//fetch video api
+function fetchVideos(videos){
+    const options = {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-key': 'eea6a34f4dmsh296963a5654d19ep13d93ajsn94fa453c501f',
+          'x-rapidapi-host': 'youtube138.p.rapidapi.com'
+        }
+      };
+    const url = "https://youtube138.p.rapidapi.com/v2/trending";
+
+    axios.get(url, options).then(response => renderVideos(response.data.list)).catch(error => console.error('Error fetching videos', error));
+}
+
+//defining variables
+let videoContainer = document.querySelector('.videos');
+
+//function for rendering videos
+function renderVideos(videos){
+    videoContainer.innerHTML = '';
+    console.log(videos);
+    videos.forEach((video) => {
+        const vidItem = document.createElement('div');
+        vidItem.classList.add('video');
+        vidItem.innerHTML = 
+                `
+                    <div class="thumbnail">
+                    <img src="${video.videoThumbnails[0].url || '' }" alt="">
+                    </div>
+                    <!-- end of thumbnail -->
+                    <div class="details">
+                    <div class="author">
+                    <img src="${video.authorThumbnails[0].url || '' }" alt="">
+                    </div>
+                    <!-- end of author -->
+                    <div class="title">
+                    <h3>${video.title || 'No Title' }</h3>
+                    <a href="${video.authorUrl}">${video.author}</a>
+                    <span>${video.viewCountText || 'No Views'} • ${video.publishedText || 'Unknown Date'}</span>
+                    </div>
+                    <!-- end of title -->
+                    </div>
+                    <!-- end of details -->
+          `;
+          videoContainer.appendChild(vidItem);
+    })
+    
+}
